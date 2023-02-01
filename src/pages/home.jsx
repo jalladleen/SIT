da0f0f5fb04c483899404010111234317a29acde
6660-Kp <60719433+6660-Kp@users.noreply.github.com>
2023-02-01 13:12:02 -0500

    navbar+footer by xinhe and andrew

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,25 +1,14 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
-
 const home = () => {
-  return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+  return (  
+    <div>
+      
+    home
+      
 
-      <Outlet />
-    </>
+     
+    </div>
   )
 };
 
