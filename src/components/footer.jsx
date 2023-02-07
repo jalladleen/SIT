ba1d81e67a144b341056d421835a59b46ddcc99c
6660-Kp <60719433+6660-Kp@users.noreply.github.com>
2023-02-07 10:30:22 -0500

    nav-footer-xinhe and andrey

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 import "./footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faFacebook,
+	faTwitter,
+	faInstagram
+  } from "@fortawesome/free-brands-svg-icons";
+import  { useState } from 'react';
 
 const Footer =()=> {
+	const [email, setEmail] = useState('');
+	const [subscribed, setSubscribed] = useState(false);
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		// Placeholder for email subscription logic
+		console.log(`Subscribed email: ${email}`);
+		setSubscribed(true);
+	  };
   return (
     
   <div class="container">
@@ -10,72 +25,84 @@ const Footer =()=> {
     	    <footer class="footer">				
 		<div class="container">
 			<div class="row">
-				<div class="col-md-3 m-b-30">
-					<div class="footer-title m-t-5 m-b-20 p-b-8">
+				<div class="col-sm">
+					<div class="footer-title m-t-5 m-b-30 p-b-8">
 						About us
 					</div>	
 					<p class="white-text">
-						xxxxxx
+						it is a long established fact that a reader will be distracted by the readable content of a page when look at 
 					</p>
-				</div>
-				<div class="col-md-3 m-b-30">
-					<div class="footer-title m-t-5 m-b-20 p-b-8">
-						xxx
-					</div>	
-					<div class="footer-links">
-						<a href="#">
-							xxxx
-						</a>
-						<a href="#">
-							xxx
-						</a>
-
-					</div>
-				</div>
-				<div class="col-md-3 m-b-30">
-					<div class="footer-title m-t-5 m-b-20 p-b-8">
-						xxx
-					</div>	
-					<div class="footer-links">
-						<a href="#">
-							xxx
-						</a>
-						<a href="#">
-							xxx
-						</a>
-
-					</div>
-				</div>
-				<div class="col-md-3 m-b-30">
-					<div class="footer-title m-t-5 m-b-20 p-b-8">
-						xxxx
-					</div>	
-					<div class="footer-links">
-						<a href="#">
-							xxx
-						</a>
-						<a href="#">
-							xxx
-						</a>
-
-					</div>
-
 					<div class="footer-social-links m-t-30">
 						<li>
-							<a href="#">
-								<i class="fa fa-facebook" aria-hidden="true"></i>
-							</a>
-							<a href="#">
-								<i class="fa fa-twitter" aria-hidden="true"></i>
-							</a>
-							<a href="#">
-								<i class="fa fa-linkedin" aria-hidden="true"></i>
-							</a>
-							<a href="#">
-								<i class="fa fa-youtube" aria-hidden="true"></i>
-							</a>
+						<a href="https://www.facebook.com//"
+						className="facebook social">
+						<FontAwesomeIcon icon={faFacebook} size="2x" />
+					</a>
+					<a href="https://www.twitter.com/" className="twitter social">
+						<FontAwesomeIcon icon={faTwitter} size="2x" />
+					</a>
+					<a href="https://www.instagram.com/"
+						className="instagram social">
+						<FontAwesomeIcon icon={faInstagram} size="2x" />
+					</a>
 						</li>
 					</div>
+				</div>
+				<div class="col-sm">
+					<div class="footer-title m-t-5 m-b-30 p-b-8">
+						Course
+					</div>	
+					<div class="footer-links">
+						<a href="#">
+							Web Design/Development
+						</a>
+						<a href="#">
+							App Development
+						</a>
+						<a href="#">
+							UI/UX Design
+						</a>
+						<a href="#">
+							HubSpot Integration
+						</a>
+						<a href="#">
+							Email Marketing
+						</a>
+						<a href="#">
+							Website Migration
+						</a>
+
+					</div>
+				</div>
+				
+				<div class="col-sm">
+					<div class="footer-title m-t-5 m-b-20 p-b-8">
+						Subscribe Us
+					</div>	
+					<div class="footer-links">
+						<a href="#">
+							it is a long established fact that a reader will be distracted by the readable
+						</a>
+						{subscribed ? (
+						<p>Thank you for subscribing!</p>
+					) : (
+						<form onSubmit={handleSubmit}>
+						<input
+							type="email"
+							placeholder="Enter your email"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+							required
+						/><br />
+						<button style={{   color: "white" , backgroundColor: "#ff304d" }} type="submit">
+         		Subscribe 
+        </button>
+						</form>
+					)}
+
+					</div>
+
+					
 				</div>
 			</div>
 		</div>
