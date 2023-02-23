9640bb08f1ad615adeb27e3e55b790210877ef8e
laljall <laljall@uwo.ca>
2023-02-23 16:25:45 -0500

    Committing changes before switching branches

--- src/pages/publicEvents.jsx
+++ src/pages/publicEvents.jsx
@@ -1,1 +1,16 @@
 
+import React from "react";
+const PublicEvents = () => {
+return (
+
+<div>
+	<br /><br />
+	<center>	<h1>public events Page</h1>
+	</center>
+	<br /><br />
+</div>
+	
+);
+};
+
+export default PublicEvents;
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,1 +1,14 @@
 
+
+import React from "react";
+const Register = () => {
+return (
+	<div>
+		<br /><br />
+		<center><h1>Register Page</h1></center>
+		<br /><br />
+	</div>
+);
+};
+
+export default Register;
