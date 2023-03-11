47dd165b632d5dddcce139a823eb84d62879c73c
laljall <laljall@uwo.ca>
2023-03-11 17:33:22 -0500

    added socials on contact and deleted twitter from footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -38,9 +38,6 @@ const Footer = () => {
                       <a href="https://www.facebook.com//" className="facebook social">
                         <FontAwesomeIcon icon={faFacebook} size="2x" />
                       </a>
-                      <a href="https://www.twitter.com/" className="twitter social">
-                        <FontAwesomeIcon icon={faTwitter} size="2x" />
-                      </a>
                       <a href="https://www.instagram.com/" className="instagram social">
                         <FontAwesomeIcon icon={faInstagram} size="2x" />
                       </a>
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,6 +1,14 @@
 import React, { useRef, useState, useEffect } from 'react';
 import emailjs from 'emailjs-com';
 import "./contact.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons';
+
 
 const Contact = () => {
   const form = useRef();
@@ -44,8 +52,8 @@ const Contact = () => {
 
         >\ Get In Touch \</h6>
         <h4 className="learn" style={{ color: "#0E1F51", textAlign: "center", font: "Raleway" }}>Hey! Let's Talk</h4>
-        <div className='container' style={{ display: "flex", width: "1300px" }}>
-          <div className="form-container" >
+        <div className='container' style={{ display: "flex"}}>
+          <div className="form-container" style={{ marginRight: "-100px"}}>
             {isMessageVisible ?
               <div className="success-message">
                 <p>Message sent!</p>
@@ -53,7 +61,7 @@ const Contact = () => {
               </div> :
               null
             }
-            <form ref={form} onSubmit={sendEmail} >
+            <form ref={form} onSubmit={sendEmail}  >
               <input type="text" name="user_fname" placeholder="First Name" style={{ background: "#f7f7f7" }} required />
 
               <input type="text" name="user_lname" placeHolder="Last Name" style={{ background: "#f7f7f7" }} required />
@@ -68,19 +76,31 @@ const Contact = () => {
               <input type="submit" value="Send Now" style={{ width: "150px", padding: "15px" }} />
             </form>
           </div>
-          <div className="info">
+          <div className="info" >
             <div>
-              <h3>send Email</h3>
-              <h6>enter email here</h6>
-              <br></br>
 
-              <h3>Follow us</h3>
+              <center><h3>Follow us</h3></center>
+              <br />
 
-              <div className="socials">
-                <button style={{ color: "white", backgroundColor: "#ff304d" }} type="submit"></button>
-                <button style={{ color: "white", backgroundColor: "#ff304d" }} type="submit"></button>
-                <button style={{ color: "white", backgroundColor: "#ff304d" }} type="submit"></button>
+              <div className="socials" style={{display:"flex"}}>
+                <button style={{ color: "white", backgroundColor: "#ff304d" }} type="submit">
+                  <a href="https://www.facebook.com//" className="facebook social"  style={{color: "white"}}>
+                        <FontAwesomeIcon icon={faFacebook} size="2x" />
+                  </a>
+                </button>
+
+                
+                <button style={{ color: "white", backgroundColor: "#ff304d" }} type="submit">
+                  <a href="https://www.instagram.com/" className="instagram social"  style={{color: "white"}}>
+                        <FontAwesomeIcon icon={faInstagram} size="2x" />
+                  </a>
+                </button>
+
               </div>
+              <br /><br />
+
+              <h5><a href =  "mailto: seniorsinit.club@westernusc.ca">Send Email To SIT</a></h5>
+
 
             </div>
 
