aa72b23c5c488093877d8b0fa5d88421657e9596
laljall <laljall@uwo.ca>
2023-03-12 15:18:33 -0400

    added routes on the app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import NavbarComp from "./components/navbar";
 import Footer from "./components/footer";
 import Subscribe from "./pages/subscribe"; // import the Subscribe component
 
+import Courses from '../pages/courses';
+import Home from '../pages/home';
+import Contact from '../pages/contact';
+import About from '../pages/about';
+import Register from '../pages/register';
+import PublicEvents from '../pages/publicEvents';
+
 function App() {
   return (
     <div className="App">
@@ -13,6 +20,12 @@ function App() {
           <NavbarComp />
           <Routes>
             <Route path="/subscribe" element={<Subscribe />} /> 
+            <Route element={<About></About>} path="/about" />
+                        <Route element={<Home></Home>} path="/home" />
+                        <Route element={<Contact></Contact>} path="/contact" />
+                        <Route element={<Courses></Courses>} path="/courses" />
+                        <Route element={<Register></Register>} path="/register" />
+                        <Route element={<PublicEvents></PublicEvents>} path="/publicEvents" />
           </Routes>
           <Footer />
         </div>
